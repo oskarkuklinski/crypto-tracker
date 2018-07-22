8ba5bbc9b3aac2b4d4b6be48bc647edf48a04abf
oskarkuklinski <oskar.kuklinski@wp.pl>
2018-07-22 17:57:40 +0100

    Added new API from cryptocompare.com to get logos of cryptocurrency

--- cryptoController.js
+++ cryptoController.js
@@ -1,8 +1,9 @@
 const request = require('request');
 
 exports.crypto_list = function(req, res) {
+    // coinmarketcap.com API request to get a list of cryptocurrencies
     request({
-        url: 'https://api.coinmarketcap.com/v1/ticker/?limit=50',
+        url: 'https://api.coinmarketcap.com/v1/ticker/?limit=20',
         json: true,
         method: "GET",
         timeout: 10000,
@@ -13,11 +14,44 @@ exports.crypto_list = function(req, res) {
             return console.error('upload failed: ', error);
         }
         // Uploaded successfully
-        res.render('index', {title: "Check your cryptocurrency!", cryptos: body });
+        var cryptos = body;
+        var cryptoSymbols = [];
+        cryptos.forEach(function(crypto) {
+            cryptoSymbols.push(crypto.symbol);
+        });
+        
+        // Another request for a cryptocompare.com to get the logos of cryptocurrencies
+        request({
+            url: 'https://www.cryptocompare.com/api/data/coinlist/',
+            json: true,
+            method: "GET",
+            timeout: 10000,
+            followRedirect: true,
+            maxRedirects: 10
+        },(error, response, body) => {
+            if (error) {
+                return console.error('upload failed: ', error);
+            }
+            // Uploaded successfully
+            // Take an image link from each of the crypto symbols listed
+            // eval() changes string to an expression that allows for reaching the image link
+            var cryptoSymbolUrls = [];
+            cryptoSymbols.forEach(function(symbol) {
+                if (eval("body.Data."+symbol) != undefined) {
+                    cryptoSymbolUrls.push(eval("body.Data."+symbol+".ImageUrl"));
+                    console.log(cryptoSymbolUrls);
+                } else {
+                    cryptoSymbolUrls.push("No existing logo");
+                    console.log("undefined");
+                }
+            });
+            res.render('index', {title: "Check your cryptocurrency!", cryptos, cryptoSymbolUrls });
+        });
     });
 };
 
 exports.crypto_details = function(req, res) {
+    // coinmarketcap.com request to get information about a specific coin
     request({
         url: 'https://api.coinmarketcap.com/v1/ticker/' + req.params.cryptoId + '/',
         json: true,
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const request = require('request');
 const app = express();
 
