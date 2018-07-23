9a00a76fcd279307c94bf88d0fc63e5cebcd368b
oskarkuklinski <oskar.kuklinski@wp.pl>
2018-07-23 23:13:33 +0100

    Added mongoDB connection and schema for user login

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
+require('dotenv').load();
 const express = require('express');
 const request = require('request');
+const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 
-// require a file with CryptoMarketCap API
+// require a file with the API
 const crypto = require('./cryptoController.js');
 
+// Connect to MongoDB
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds145881.mlab.com:45881/crypto-tracker`, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('MongoDB connected!');   
+    }
+});
+
 // set pug as a view engine
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
 
 // path for static client files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', crypto.crypto_list);
 
-app.get('/:cryptoId', crypto.crypto_details);
+app.get('/crypto/:cryptoId', crypto.crypto_details);
+
+app.get('/register', function(req, res) {
+    console.log('register page');
+});
+
+app.get('/login', function(req, res) {
+    console.log('login page');
+});
 
 // Catch 404 error
 app.use((req, res, next) => {
